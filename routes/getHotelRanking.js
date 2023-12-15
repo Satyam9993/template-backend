fcd8c625cac54a3db4967a9c46731245de47d2d8
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-12-15 21:05:22 +0530

    updating template

--- routes/getHotelRanking.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import {getHotelRankingData} from "../controller/Ranking.js"
-const router = express.Router();
-
-router.get('/hotel-rank', getHotelRankingData);
-
-export default router
